f2ce5f937a9b3fcb303927e040714349f71b74bf
Shawn Lewis <shlewis@gmail.com>
2017-01-05 19:18:32 -0800

    svm start code.

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -26,31 +26,27 @@ def svm_loss_naive(W, X, y, reg):
   num_train = X.shape[0]
   loss = 0.0
   for i in xrange(num_train):
-    scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
+    scores = X[i].dot(W)  # X[i] = (D,) scores = (C,)
+    correct_class_score = scores[y[i]] # scalar
     for j in xrange(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, y[i]] -= X[i]
+        dW[:, j] += X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
 
+  dW /= num_train
+
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Compute the gradient of the loss function and store it dW.                #
-  # Rather that first computing the loss and then computing the derivative,   #
-  # it may be simpler to compute the derivative at the same time that the     #
-  # loss is being computed. As a result you may need to modify some of the    #
-  # code above to compute the gradient.                                       #
-  #############################################################################
-
+  dW += reg*W
 
   return loss, dW
 
@@ -58,6 +54,13 @@ def svm_loss_naive(W, X, y, reg):
 def svm_loss_vectorized(W, X, y, reg):
   """
   Structured SVM loss function, vectorized implementation.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
 
   Inputs and outputs are the same as svm_loss_naive.
   """
@@ -69,7 +72,15 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  N = X.shape[0]
+  scores = X.dot(W) # (N, C)
+  correct_scores = scores[np.arange(N), y]
+  inner = (scores.T - correct_scores + 1).T
+  inner[inner < 0] = 0
+  loss = np.sum(inner, axis=1) - 1 # handle j=yi
+  loss = np.sum(loss) / N
+  #TODO: handle regularization
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
